c78158d449ca92eeef42cb6a950a9915c2f45f57
hit300b1 <hit300b1@gmail.com>
2022-03-16 15:09:30 +0530

    16-mar

--- sirius2022/src/day3/StaticDemo.java
+++ sirius2022/src/day3/StaticDemo.java
@@ -2,13 +2,18 @@ package day3;
 
 public class StaticDemo {
 	public static void main(String[] args) {
-		StaticDemo obj=new StaticDemo();
-		obj.met1();
+//		StaticDemo obj=new StaticDemo();
+//		obj.met1();
+		StaticDemo.met();
+		StaticDemo.met2();
 	}
 	void met1() {
 		
 	}
 	static void met() {
+		
+	}
+	static void met2() {
 		
 	}
 }
