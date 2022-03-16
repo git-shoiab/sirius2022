9c510cd5e53e1417d405305555dc84eff8b03181
hit300b1 <hit300b1@gmail.com>
2022-03-16 15:16:00 +0530

    16-mar

--- sirius2022/src/day3/StaticDemo.java
+++ sirius2022/src/day3/StaticDemo.java
@@ -1,19 +1,26 @@
 package day3;
-
+//https://fluvid.com/videos/detail/37jwQhOYk4Iy1_Rxg#.YjGxsPNAn4Q.link
 public class StaticDemo {
+	public StaticDemo() {
+		System.out.println("cons called....");
+	}
+	static {
+		System.out.println("static block called...");
+	}
 	public static void main(String[] args) {
-//		StaticDemo obj=new StaticDemo();
+		StaticDemo obj=new StaticDemo();
 //		obj.met1();
-		StaticDemo.met();
+		obj.met2();
+		StaticDemo.met();StaticDemo.met();
 		StaticDemo.met2();
 	}
 	void met1() {
 		
 	}
 	static void met() {
-		
+		System.out.println("static met called...");
 	}
 	static void met2() {
-		
+		System.out.println("static met2 called...");
 	}
 }
