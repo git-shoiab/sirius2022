b05de9005572dce5e6806ebd2565198110f3d9d8
hit300b1 <hit300b1@gmail.com>
2022-03-21 14:37:43 +0530

    21-mar

--- sirius2022/src/exceppack/ExcepDemo4.java
+++ sirius2022/src/exceppack/ExcepDemo4.java
@@ -4,15 +4,22 @@ https://fluvid.com/videos/detail/n8QdniKVQkSXBx3Gg#.Yjgi4TN4gLY.link
  */
 package exceppack;
 
+import java.util.Scanner;
+
 /**
  * @author Hp
  *
  */
 public class ExcepDemo4 {
-	public static void main(String[] args) {
+	public static void main(String[] args)throws Exception {
 		Child baby=new Child();
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the item...:");
+		String itemclass=scan.next();
+		Item item=(Item)Class.forName(itemclass).getConstructor().newInstance();
 		Dog tiger=new Dog();
-		baby.playWithDog(new Stick(), tiger);
+		baby.playWithDog(item, tiger);
 	}
 }
 
@@ -23,18 +30,35 @@ class Handler911{
 	public void help(DogBarkException dbr) {
 		System.out.println("barking dogs seldom bite...relax.........");
 	}
+	public void help(DogPlayException dbr) {
+		System.out.println("happy dogs dont bite...play.........");
+	}
 }
 abstract class Item{
 	public abstract void action()throws DogExceptions;
 }
 class Stick extends Item{
+	public Stick() {
+		// TODO Auto-generated constructor stub
+	}
 	public void action() throws DogExceptions{
 		throw new DogBiteException("you beat I bite....");
 	}
 }
 class Stone extends Item{
+	public Stone() {
+		// TODO Auto-generated constructor stub
+	}
 	public void action() throws DogExceptions{
 		throw new DogBarkException("you hit, I chase....");
+	}
+}
+class Ball extends Item{
+	public Ball() {
+		// TODO Auto-generated constructor stub
+	}
+	public void action() throws DogExceptions{
+		throw new DogPlayException("you throw, I catch....");
 	}
 }
 
@@ -49,12 +73,13 @@ class Child{
 			dog.play(item);
 		}catch(DogExceptions dee) {
 			System.out.println(dee);
+			dee.visit();
 		}
 	}
 }
 
 abstract class DogExceptions extends Exception{
-	
+	public abstract void visit();
 }
 class DogBiteException extends DogExceptions{
 	String msg;
@@ -64,6 +89,10 @@ class DogBiteException extends DogExceptions{
 	@Override
 	public String toString() {
 		return "Exception....: "+msg;
+	}
+	@Override
+	public void visit() {
+		new Handler911().help(this);
 	}
 }
 
@@ -75,5 +104,23 @@ class DogBarkException extends DogExceptions{
 	@Override
 	public String toString() {
 		return "Exception....: "+msg;
+	}
+	@Override
+	public void visit() {
+		new Handler911().help(this);
+	}
+}
+class DogPlayException extends DogExceptions{
+	String msg;
+	public DogPlayException(String msg) {
+		this.msg=msg;
+	}
+	@Override
+	public String toString() {
+		return "Exception....: "+msg;
+	}
+	@Override
+	public void visit() {
+		new Handler911().help(this);
 	}
 }
