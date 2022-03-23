652235d64eb413954adfff7169b07fef14e88e65
hit300b1 <hit300b1@gmail.com>
2022-03-23 13:01:48 +0530

    22-mar

---
+++ sirius2022/src/annopack/AnnoDemo.java
@@ -0,0 +1,70 @@
+package annopack;
+
+public class AnnoDemo {
+	public static void main(String[] args) {
+		ShoeShop shop=ChennaiContainer.getShoeShop();
+		
+		System.out.println(shop.sellShoe());
+	}
+}
+
+class ChennaiContainer{
+	
+	public static ShoeShop getShoeShop() {
+		ShoeFactory factory=new LakhaniShoeFactory();
+		ShoeShop shop=new SiriusShoeShop();
+		
+		//knowledge of factory to shop - because shop is dependent on factory
+		
+		shop.setFactory(factory);
+		
+		return shop;
+	}
+}
+abstract class Shoe{}
+
+class LeatherShoe extends Shoe{}
+class SportsShoe extends Shoe{}
+
+interface Manufacturer{
+	public Shoe makeShoe();
+}
+abstract class ShoeFactory implements Manufacturer{
+	
+}
+interface Seller{
+	public Shoe sellShoe();
+}
+abstract class ShoeShop implements Seller{
+	private ShoeFactory factory;
+
+	public ShoeFactory getFactory() {
+		return factory;
+	}
+
+	public void setFactory(ShoeFactory factory) {
+		this.factory = factory;
+	}
+	
+}
+
+class BataShoeFactory extends ShoeFactory{
+	@Override
+	public Shoe makeShoe() {
+		return new LeatherShoe();
+	}
+}
+class LakhaniShoeFactory extends ShoeFactory{
+	@Override
+	public Shoe makeShoe() {
+		return new SportsShoe();
+	}
+}
+
+class SiriusShoeShop extends ShoeShop{
+	@Override
+	public Shoe sellShoe() {
+		// TODO Auto-generated method stub
+		return getFactory().makeShoe();
+	}
+}
