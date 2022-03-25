e5125546ed29205d34e9fd95a9fe7682b3766d0e
hit300b1 <hit300b1@gmail.com>
2022-03-25 15:56:55 +0530

    25-mar

--- sirius2022/src/iopack/IODemo3.java
+++ sirius2022/src/iopack/IODemo3.java
@@ -1,5 +1,5 @@
 package iopack;
-
+//https://fluvid.com/videos/detail/-KRkYhGkG6HEzoo3p#.Yj2Y11_foQM.link
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
@@ -7,15 +7,18 @@ import java.io.FileWriter;
 
 public class IODemo3 {
 	public static void main(String[] args)throws Exception {
+		try(
 		FileReader fis=new FileReader("data.txt");
 		FileWriter fos=new FileWriter("copy.txt");
-		
+				){
 		int n=0;
 		char b[]=new char[4];
 		while((n=fis.read(b))!=-1) {
 			fos.write(b,0,n);
 		}
 		
-		fos.close();
+		}catch(Exception e) {
+			
+		}
 	}
 }
