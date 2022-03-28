1eabc82c488601886c2c3b684a89eeda44b5829c
hit300b1 <hit300b1@gmail.com>
2022-03-28 10:08:12 +0530

    23-MAR

--- sirius2022/src/iopack/Client.java
+++ sirius2022/src/iopack/Client.java
@@ -1,13 +1,27 @@
 package iopack;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
-	Socket s;
+	Socket s;BufferedReader in,keyin;PrintWriter out;
 	public Client() {
 		try {
+			while(true) {
 			s=new Socket("localhost",2000);
-			System.out.println(s);
+			
+			in=new BufferedReader(new InputStreamReader(s.getInputStream()));
+			String msgFromServer=in.readLine();
+			System.out.println("Message From Server..:"+msgFromServer);
+			
+			out=new PrintWriter(s.getOutputStream(),true);
+			keyin=new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("Pls enter message for server..:");
+			String msgToServer=keyin.readLine();
+			out.println(msgToServer);
+			}
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
--- sirius2022/src/iopack/Server.java
+++ sirius2022/src/iopack/Server.java
@@ -1,17 +1,29 @@
 package iopack;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 	ServerSocket ss;Socket s;
+	PrintWriter out;BufferedReader in,socketin;
 	public Server() {
 		try {
 			ss=new ServerSocket(2000);
 			System.out.println("Server Ready...");
 			while(true) {
 				s=ss.accept();
-				System.out.println(s);
+				out=new PrintWriter(s.getOutputStream(),true);
+				in=new BufferedReader(new InputStreamReader(System.in));
+				System.out.println("Please enter a message to client...:");
+				String msgToClient=in.readLine();
+				out.println(msgToClient);
+				
+				socketin=new BufferedReader(new InputStreamReader(s.getInputStream()));
+				String msgFromClient=socketin.readLine();
+				System.out.println("Message From Client..:"+msgFromClient);
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
